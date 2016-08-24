295c8165f7812c39fc363ca2e654c8bdf70a1038
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2016-08-24 12:36:19 -0700

    code demo in class

--- class_methods.rb
+++ class_methods.rb
@@ -26,13 +26,26 @@
 ###################################
 
 ## INSTANCE VARIABLES
-
+  ## ATTRIBUTES
+      #ex: name, username, password
+#Change
 ## INSTANCE METHODS
+    #Behaviors
+      #ex: make a post, send a message, like a post, create a photo albumn
 
-      #INITIALIZE METHOD
+#INITIALIZE METHOD
+class User
+  def initalize
+    @name
+  end
 
+  def add_username(name)
+    @name = name
+  end
+end
 
-## ATTRIBUTES
+# user1 = User.new
+# puts user1.methods
 
 
 # class Order
@@ -63,48 +76,58 @@
 #Class methods are called directly by the class and not by an instance of the class.
 
 
+class Kitten
 
+  def self.say_meow
+    return "meow"
+  end
 
+end
+
+puts Kitten.say_meow
 
 ###################################
 ######    LETS TRY IT OUT!    #####
 ###################################
 
 
-# class Pawn
-#   attr_reader :position
-#   def initialize(position)
-#     @position = position
-#   end
-#
-#   # This is the class method, it starts with self.
-#   # It is only called on the class directly Pawn.make_row
-#   def self.make_row(side)
-#     if side == "white"
-#       num = 2
-#     else
-#       num = 7
-#     end
-#
-#     pawns = []
-#     ("a".."h").each do |letter|
-#       pawns << self.new("#{letter}#{num}")
-#     end
-#
-#     pawns
-#   end
+class Pawn
+  attr_reader :position
+  def initialize(position)
+    @position = position
+  end
+
+  # This is the class method, it starts with self.
+  # It is only called on the class directly Pawn.make_row
+  def self.make_row(side)
+    if side == "white"
+      num = 2
+    else
+      num = 7
+    end
+
+    pawns = []
+    ("a".."h").each do |letter|
+      pawns << self.new("#{letter}#{num}")
+    end
+
+    pawns
+  end
+end
+
+#make one pawn
+one_pawn = Pawn.new("A2")
+one_pawn.new("B4")
+
+#make a whole row of pawns
+#pawns = Pawn.make_row("orange")
+
+#What is being stored in this local variable pawns?
+#  pawns.each do |pawn|
+#   puts pawn.position
 # end
-#
-# #make one pawn
-# one_pawn = Pawn.new("A2")
-#
-# #make a whole row of pawns
-# pawns = Pawn.make_row("black")
-#
-# #What is being stored in this local variable pawns?
-# print pawns
-#
-# #WHAT IS THIS DOING!?
+
+#WHAT IS THIS DOING!?
 # puts pawns.shuffle.first.position
 
 
@@ -116,7 +139,7 @@
 
 
 #In Gems, like faker
-  # https://github.com/stympy/faker/blob/master/lib/faker/hacker.rb
+  https://github.com/stympy/faker/blob/master/lib/faker/hacker.rb
 
 #When we get into databases, our data will be tied to a class. That class will have some premade class methods for us to use:  .find, .last, .where
 #Those class methods allow you to find specific objects of that class based on an specified attributes of that class.
